b1151802dc324c8d62013a26c679349338d43ac8
jiweon <kor98won@gmail.com>
2021-04-07 18:10:01 +0900

    week06 PA2

--- week06/PA2_2.c
+++ week06/PA2_2.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 typedef struct {
-    int t;
-    int idx;
+    int st;
+    int en;
 } Time;
 
 int N;
-int st_org[500005];
-Time en[500005];
+Time t[500005];
 Time tmp[500005];
 
 void merge(int l, int m, int r) {
@@ -16,17 +15,23 @@ void merge(int l, int m, int r) {
     while (idx <= r) {
         // 둘중 하나를 모두 배치했을 때, 남은 하나를 마저 배치해줘야 한다.
         if (i1 > m) // l ~ m 배열을 모두 배치했다면
-            tmp[idx++] = en[i2++];
+            tmp[idx++] = t[i2++];
         else if (i2 > r) // m + 1 ~ r 배열을 모두 배치했다면
-            tmp[idx++] = en[i1++];
+            tmp[idx++] = t[i1++];
         // 둘 다 배치가 아직 안됐을 경우
-        else if (en[i1].t <= en[i2].t) // l ~ m 배열의 다음 원소가 더 작다면 먼저 배치
-            tmp[idx++] = en[i1++];
+        else if (t[i1].en == t[i2].en) {
+            if (t[i1].st <= t[i2].st)
+                tmp[idx++] = t[i1++];
+            else
+                tmp[idx++] = t[i2++];
+        }
+        else if (t[i1].en < t[i2].en) // l ~ m 배열의 다음 원소가 더 작다면 먼저 배치
+            tmp[idx++] = t[i1++];
         else // m + 1 ~ r 배열의 다음 원소가 더 작다면 먼저 배치
-            tmp[idx++] = en[i2++];
+            tmp[idx++] = t[i2++];
     }
     for (int i = l; i <= r; i++)
-        en[i] = tmp[i];
+        t[i] = tmp[i];
 }
 
 void mergeSort(int l, int r) {
@@ -43,9 +48,8 @@ int getCheckCnt() {
     for (int i = 0; i < N; i++) {
         // 더 늦게 나가는 사람이 더 먼저 왔으면 겹침.
         int j = i;
-        while (j < N && st_org[en[j].idx] <= en[i].t) {
+        while (j < N && t[j].st <= t[i].en)
             j++;
-        }
         i = j - 1;
         cnt++;
     }
@@ -54,10 +58,8 @@ int getCheckCnt() {
 
 int main(void) {
     scanf(" %d", &N);
-    for (int i = 0; i < N; i++) {
-        scanf(" %d %d", &st_org[i], &en[i].t);
-        en[i].idx = i;
-    }
+    for (int i = 0; i < N; i++)
+        scanf(" %d %d", &t[i].st, &t[i].en);
     mergeSort(0, N - 1);
     printf("%d", getCheckCnt());
     return 0;
